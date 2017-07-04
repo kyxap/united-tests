3d925fa353e84c4cee253b053f8992f0b22020c1
Roman Kukharuk <rkukharuk@gmail.com>
2017-07-04 11:41:05 -0700

    :robot: refactor + new text verification

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,10 +1,12 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class HomePage {
     private WebDriver driver;
+    private SearchFlightsPage searchFlightsPage;
     private By fromInputField = By.id("Origin");
     private By toInputField = By.id("Destination");
     private By departDate = By.cssSelector("#DepartDate");
@@ -17,35 +19,48 @@ public class HomePage {
         this.action = new Actions(this.driver);
     }
 
-    public boolean trueTest() {
+    public boolean fillFlightDates(String dep, String ret) {
+        WebElement departDateElement = driver.findElement(departDate);
+        WebElement returnDateElement = driver.findElement(returnDate);
+        departDateElement.sendKeys(dep);
+        returnDateElement.sendKeys(ret);
+        //TODO: state verification
         return true;
     }
-    public void actionMoveClickSendKey(WebElement element, String input) {
-        this.action.moveToElement(element).click().sendKeys(input).build().perform();
+
+    public boolean fillFromTo(String from, String to) {
+        WebElement fromInputFieldElement = driver.findElement(fromInputField);
+        WebElement toInputFieldElement = driver.findElement(toInputField);
+        fromInputFieldElement.sendKeys(from);
+        toInputFieldElement.sendKeys(to);
+        
+        //TODO: state verification
+        return true;
+    }
+
+    public SearchFlightsPage submitSearchFormBy (WebElement element) {
+        if (element == null) {
+            WebElement fromInputFieldElement = driver.findElement(fromInputField);
+            fromInputFieldElement.sendKeys(Keys.ENTER);
+        } else element.sendKeys(Keys.ENTER);
+        return new SearchFlightsPage(this.driver);
     }
 
     public SearchFlightsPage searchFlightsByShortName(IATA from, IATA to, String dep, String ret){
-        WebElement departDateElement = driver.findElement(departDate);
-        WebElement returnDateElement = driver.findElement(returnDate);
-        actionMoveClickSendKey(departDateElement, dep);
-        actionMoveClickSendKey(returnDateElement, ret);
-        WebElement fromInputFieldElement = driver.findElement(fromInputField);
-        WebElement toInputFieldElement = driver.findElement(toInputField);
-        actionMoveClickSendKey(fromInputFieldElement, from.getShort());
-        actionMoveClickSendKey(toInputFieldElement, to.getShort());
-        WebElement submitButtonElement = this.driver.findElement(submitButton);
-        submitButtonElement.click();
-        return new SearchFlightsPage(driver);
+        if (fillFlightDates(dep, ret) && fillFromTo(from.getShort(), to.getShort())) {
+            this.searchFlightsPage = submitSearchFormBy(null);
+        } else this.searchFlightsPage = null;
+        return searchFlightsPage;
     }
 
     public String getPageTitle(){
         String title = driver.getTitle();
-//        System.out.println(">>>>>"+title);
         return title;
     }
 
     public boolean verifyMainPageTitle() {
         String expectedTitle = "United Airlines – Airline Tickets, Travel Deals and Flights";
+        System.out.println(getPageTitle());
         return getPageTitle().contains(expectedTitle);
     }
 }
--- src/test/java/SearchFlightsTest.java
+++ src/test/java/SearchFlightsTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SearchFlightsTest extends BaseTest {
+public class SearchFlightsTest extends TestSetup {
     private WebDriver driver;
     HomePage homePage;
     SearchFlightsPage searchFlightsPagePage;
@@ -12,21 +12,28 @@ public class SearchFlightsTest extends BaseTest {
     @BeforeClass
     public void setUp() {
         driver = getDriver();
-    }
 
-    @Test(priority = 1)
-    public void searchFlightByShortNamePDXtoLAX() {
-        HomePage homePage = new HomePage(driver);
+        this.homePage = new HomePage(driver);
         searchFlightsPagePage = homePage.searchFlightsByShortName(IATA.PDX, IATA.LAX, "Dec 15 2017", "Dec 30 2017");
     }
 
-    @Test(priority = 2)
+    @Test()
     public void findFullNameOfDestination() {
         Assert.assertTrue(searchFlightsPagePage.findContainsText(IATA.LAX.getFull()), "Unable to find " + IATA.LAX.getFull());
     }
 
-    @Test(priority = 2)
+    @Test()
     public void findFullNameOfDeparture() {
         Assert.assertTrue(searchFlightsPagePage.findContainsText(IATA.PDX.getFull()), "Unable to find " + IATA.PDX.getFull());
+    }
+
+    @Test()
+    public void findShortNameOfDestination() {
+        Assert.assertTrue(searchFlightsPagePage.findContainsText(IATA.LAX.getFull()), "Unable to find " + IATA.LAX.getShort());
+    }
+
+    @Test()
+    public void findShortNameOfDeparture() {
+        Assert.assertTrue(searchFlightsPagePage.findContainsText(IATA.PDX.getFull()), "Unable to find " + IATA.PDX.getShort());
     }
 }
